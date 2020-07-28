7b408c0631eae2b433123fd7c20e69b3602f2323
Dillon Lee <u0105808@redwood4.int.chpc.utah.edu>
2020-07-28 16:30:55 -0600

    added more slivar cmds

--- run.sh
+++ run.sh
@@ -221,7 +221,7 @@ echo "$scriptDir/externals/slivar/slivar expr \
 	--vcf $vcfFile.bcftools.vcf.gz \
 	--ped $pedFile \
     --js $scriptDir/externals/slivar/slivar-functions.js \
-	--info \"INFO.gnomad_popmax_af < $gnomadAFThreshold\" \
+	--info \"INFO.gnomad_popmax_af < $gnomadAFThreshold\" &&  && variant.FILTER == \"PASS\" &&  variant.ALT[0] != \"*\" \
 	--gnotate $gnomadFile \
     --family-expr 'denovo:fam.every(segregating_denovo)' \
     --family-expr 'x_denovo:(variant.CHROM == \"X\" || variant.CHROM == \"chrX\") && fam.every(segregating_denovo_x)' \
@@ -233,7 +233,7 @@ $scriptDir/externals/slivar/slivar expr \
 	--vcf $vcfFile.bcftools.vcf.gz \
 	--ped $pedFile \
 	--js $scriptDir/externals/slivar/slivar-functions.js \
-	--info "INFO.gnomad_popmax_af < $gnomadAFThreshold" \
+	--info "INFO.gnomad_popmax_af < $gnomadAFThreshold" && variant.FILTER == "PASS" &&  variant.ALT[0] != "*" \
 	--gnotate $gnomadFile \
     --family-expr 'denovo:fam.every(segregating_denovo)' \
     --family-expr 'x_denovo:(variant.CHROM == "X" || variant.CHROM == "chrX") && fam.every(segregating_denovo_x)' \
